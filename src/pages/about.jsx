e15a12b1a6cdff57501fa03ef717a6e1832bed8e
StellaSFE96 <stella.samaranayake@iths.se>
2023-01-15 00:00:54 +0100

    finish queries for about me page and education list

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,93 +1,53 @@
 import * as React from "react";
 import * as style from "../styles/AboutMe.module.scss";
 import Layout from "../components/Layout";
+import { graphql, useStaticQuery } from "gatsby";
 
 export default function About() {
+  // Trying useStaticQuery in this specific query
+  const { contentfulAboutMe, allContentfulEducationEntry } =
+    useStaticQuery(query);
+
   return (
     <Layout>
       <article className={style.container}>
         <aside>
-          <h1>This is my About me page</h1>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur
-            temporibus similique unde natus blanditiis quis, necessitatibus,
-            repudiandae rerum voluptate harum quibusdam distinctio reiciendis
-            odit illum dolorum reprehenderit quos dignissimos laudantium?
-          </p>
+          <h1>{contentfulAboutMe.title}</h1>
+          <p>{contentfulAboutMe.description.description}</p>
         </aside>
         <section>
           <div className={style.experienceContainer}>
             <h1 className={style.experienceTitle}>Experience</h1>
-            <ul>
-              <li>
-                <div>
-                  <h1>Jobb som nånting</h1>
-                  <h2>2022-01-01 - 2022-01-01</h2>
-                </div>
-                <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure
-                  voluptas cupiditate, sapiente quis dolores delectus aperiam
-                  odio suscipit et minus veniam corrupti aspernatur hic fugit
-                  nobis. Eius, ex quia eum molestiae sunt voluptas quae, iusto
-                  sit ipsam id repellendus aspernatur?
-                </p>
-              </li>
-              <li>
-                <div>
-                  <h1>Jobb som nånting</h1>
-                  <h2>2022-01-01 - 2022-01-01</h2>
-                </div>
-                <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Similique, nobis.
-                </p>
-              </li>
-              <li>
-                <div>
-                  <h1>Jobb som nånting</h1>
-                  <h2>2022-01-01- 2022-01-01</h2>
-                </div>
-                <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Similique, nobis.
-                </p>
-              </li>
-            </ul>
+            {/* <ul>
+              {allContentfulEducationEntry.nodes.map((education) => (
+                <li key={education.educationTitle}>
+                  <div>
+                    <h1>{education.educationTitle}</h1>
+                    <h2>
+                      {education.fromDate} - {education.toDate}
+                    </h2>
+                  </div>
+                  <p>{education.shortDescription}</p>
+                  <p>{education.educationDescription}</p>
+                </li>
+              ))}
+            </ul> */}
           </div>
           <div className={style.educationContainer}>
             <h1 className={style.educationTitle}>Education</h1>
             <ul>
-              <li>
-                <div>
-                  <h1>Plugg plugg</h1>
-                  <hr />
-                  <h2>2022-01-01 - 2022-01-01</h2>
-                </div>
-                <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Similique, nobis.
-                </p>
-              </li>
-              <li>
-                <div>
-                  <h1>Plugg plugg</h1>
-                  <h2>2022-01-01 - 2022-01-01</h2>
-                </div>
-                <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Similique, nobis.
-                </p>
-              </li>
-              <li>
-                <div>
-                  <h1>Plugg plugg</h1>
-                  <h2>2022-01-01 - 2022-01-01</h2>
-                </div>
-                <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Similique, nobis.
-                </p>
-              </li>
+              {allContentfulEducationEntry.nodes.map((education) => (
+                <li key={education.educationTitle}>
+                  <div>
+                    <h1>{education.educationTitle}</h1>
+                    <h2>
+                      {education.fromDate} - {education.toDate}
+                    </h2>
+                  </div>
+                  <p>{education.shortDescription}</p>
+                  <p>{education.educationDescription}</p>
+                </li>
+              ))}
             </ul>
           </div>
         </section>
@@ -95,3 +55,23 @@ export default function About() {
     </Layout>
   );
 }
+
+export const query = graphql`
+  query AboutMeQuery {
+    contentfulAboutMe {
+      title
+      description {
+        description
+      }
+    }
+    allContentfulEducationEntry {
+      nodes {
+        educationTitle
+        shortDescription
+        educationDescription
+        fromDate(formatString: "YYYY-MM-DD")
+        toDate(formatString: "YYYY-MM-DD")
+      }
+    }
+  }
+`;
