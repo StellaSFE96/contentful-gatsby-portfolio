5ffc4a133f5928b23c995e4d8828b11321c554e9
StellaSFE96 <stella.samaranayake@iths.se>
2023-01-15 00:57:48 +0100

    add 404 page

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,19 +1,19 @@
 import * as React from "react";
 import { Link } from "gatsby";
 import Layout from "../components/Layout";
-// import * as style from "../styles/redirect.module.scss";
+import * as style from "../styles/Redirect.module.scss";
 
-const NotFoundPage = () => {
+const NotFound = () => {
   return (
     <Layout>
-      <div>
-        <h1>Page not found.</h1>
-        <Link to="/">Go back to homepage...</Link>
+      <div className={style.container}>
+        <h1>Oops! Page not found.</h1>
+        <Link className={style.homeBtn} to="/">
+          Go back...
+        </Link>
       </div>
     </Layout>
   );
 };
 
-export default NotFoundPage;
-
-export const Head = () => <title>Not found</title>;
+export default NotFound;
